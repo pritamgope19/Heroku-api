fc8084b06c3bc7530066de024c97747f2989b623
GPritam <pritamgope19@users.noreply.github.com>
2021-02-01 22:56:10 +0530

    make it better1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('./app/db/db');
+const client = require('./app/db/db');
 const user = {
     name: "Pritam Gope",
     status: "Online"
@@ -8,13 +8,42 @@ const user = {
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.pxsgh.mongodb.net/sample_training?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    if (err) {
+        return console.log('Unable to connect to Database');
+    }
 
-app.get('/users', (req, res) => {
+    console.log("Connected Successfully...");
 
-    // const user = new User(req.body);
-        res.status(200).send(user);
+
+    const collection = client.db("sample_training").collection("posts");
+    // perform actions on the collection object
+    let val = collection.find({
+        title: "Gettysburg Address"
+    }).count().then((val) => {
+        console.log(val);
+    }).catch((e) => {
+        console.log("Error: ", e);
+    });
+    app.use(express.json());
+
+    app.get('/users', (req, res) => {
+
+        // const user = new User(req.body);
+        client.client.connect
+        res.status(200).send(val);
+    })
+
+
+    client.close();
 })
+
+
+
+
 
 app.listen(port, () => {
     console.log("Server is UP! at port: ", port);
